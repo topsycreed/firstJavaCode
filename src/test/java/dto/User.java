6db429f9f882829e3530a0913f104204fc438274
gchursov <topsycreed@gmail.com>
2025-03-02 14:02:25 +0100

    Add api tests for pet store user service

---
+++ src/test/java/dto/User.java
@@ -0,0 +1,58 @@
+package dto;
+
+public class User {
+    private long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+
+    public User() {
+    }
+
+    public User(long id, String username, String firstName, String lastName, String email, String password, String phone, int userStatus) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
+}
