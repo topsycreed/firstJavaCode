463cfd8696991ca654c5087809911055aff8dd68
topsycreed <topsycreed@users.noreply.github.com>
2025-03-30 10:00:44 +0000

    Automated deployment: Sun Mar 30 10:00:44 UTC 2025 6ddabf91173b10323f833af421e6a6f650c17401

--- src/main/java/constants/AEOConstants.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class AEOConstants {
-    public static final String BASE_URL = "https://www.ae.com/us/en";
-    public static final String HOME_PAGE_TITLE = "Men’s & Women’s Jeans, Clothes & Accessories | American Eagle";
-}
--- src/test/java/apiConstants/Constants.java
+++
@@ -1,9 +0,0 @@
-package apiConstants;
-
-import dto.User;
-
-public class Constants {
-    public static final String BASE_URL = "https://petstore.swagger.io/v2";
-
-    public static final User DEFAULT_USER = new User(0, "qwerty123", "gena", "chursov", "string", "string", "string", 0);
-}
